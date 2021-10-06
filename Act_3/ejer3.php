03c4ef445a5acf89520d067264db162151fdaa6e
davidroses02 <davidrosasalcaraz@gmail.com>
2021-10-06 11:09:00 +0200

    cambio

--- Act_3/ejer3.php
+++ Act_3/ejer3.php
@@ -1,66 +1,66 @@
 <?php
-/**Escribe un programa que genere tres números aleatorios correspondientes a la fecha de
- * nacimiento (día, mes, año) de una persona. Debes garantizar que la fecha generada es válida.
- * El script mostrará en pantalla la fecha y una imagen con el signo del zodiaco de la persona.
- * @author Tomas
- */
+    /**Escribe un programa que genere tres números aleatorios correspondientes a la fecha de
+   * nacimiento (día, mes, año) de una persona. Debes garantizar que la fecha generada es válida.
+    * El script mostrará en pantalla la fecha y una imagen con el signo del zodiaco de la persona.
+    * @author Tomas
+    */
 
     
-$dia = rand(1,31);
-$mes = rand(1,12);
-$anio = rand(1940,2021);        
-$fecha = $dia."/".$mes."/".$anio;
-echo "La fecha de la persona es: ".$fecha."</br>";
+    $dia = rand(1,31);
+    $mes = rand(1,12);
+    $anio = rand(1940,2021);        
+    $fecha = $dia."/".$mes."/".$anio;
+    echo "La fecha de la persona es: ".$fecha."</br>";
 
-//Signo del zodiaco segun la fecha
-if($dia >= 21 && $mes == 3){
-    echo "Tu signo del zodiaco es: Aries";
-}elseif($dia <= 20 && $mes == 4){
-    echo "Tu signo del zodiaco es: Aries";
-}elseif($dia >= 21 && $mes == 4){
-    echo "Tu signo del zodiaco es: Tauro";
-}elseif($dia <= 20 && $mes == 5){
-    echo "Tu signo del zodiaco es: Tauro";
-}elseif($dia >= 21 && $mes == 5){
-    echo "Tu signo del zodiaco es: Géminis";
-}elseif($dia <= 20 && $mes == 6){
-    echo "Tu signo del zodiaco es: Géminis";
-}elseif($dia >= 21 && $mes == 6){
-    echo "Tu signo del zodiaco es: Cáncer";
-}elseif($dia <= 20 && $mes == 7){
-    echo "Tu signo del zodiaco es: Cáncer";
-}elseif($dia >= 21 && $mes == 7){
-    echo "Tu signo del zodiaco es: Leo";
-}elseif($dia <= 20 && $mes == 8){
-    echo "Tu signo del zodiaco es: Leo";
-}elseif($dia >= 21 && $mes == 8){
-    echo "Tu signo del zodiaco es: Virgo";
-}elseif($dia <= 20 && $mes == 9){
-    echo "Tu signo del zodiaco es: Virgo";
-}elseif($dia >= 21 && $mes == 9){
-    echo "Tu signo del zodiaco es: Libra";
-}elseif($dia <= 20 && $mes == 10){
-    echo "Tu signo del zodiaco es: Libra";
-}elseif($dia >= 21 && $mes == 10){
-    echo "Tu signo del zodiaco es: Escorpio";
-}elseif($dia <= 20 && $mes == 11){
-    echo "Tu signo del zodiaco es: Escorpio";
-}elseif($dia >= 21 && $mes == 11){
-    echo "Tu signo del zodiaco es: Sagitario";
-}elseif($dia <= 20 && $mes == 12){
-    echo "Tu signo del zodiaco es: Sagitario";
-}elseif($dia >= 21 && $mes == 12){
-    echo "Tu signo del zodiaco es: Capricornio";
-}elseif($dia <= 20 && $mes == 1){
-    echo "Tu signo del zodiaco es: Capricornio";
-}elseif($dia >= 21 && $mes == 1){
-    echo "Tu signo del zodiaco es: Acuario";
-}elseif($dia <= 20 && $mes == 2){
-    echo "Tu signo del zodiaco es: Acuario";
-}elseif($dia >= 21 && $mes == 2){
-    echo "Tu signo del zodiaco es: Piscis";
-}elseif($dia <= 20 && $mes == 3){
-    echo "Tu signo del zodiaco es: Piscis";
+    //Signo del zodiaco segun la fecha
+    if ($dia >= 21 && $mes == 3){
+        echo "Tu signo del zodiaco es: Aries";
+    } elseif ($dia <= 20 && $mes == 4) {
+        echo "Tu signo del zodiaco es: Aries";
+    }elseif($dia >= 21 && $mes == 4){
+        echo "Tu signo del zodiaco es: Tauro";
+    }elseif($dia <= 20 && $mes == 5){
+        echo "Tu signo del zodiaco es: Tauro";
+    }elseif($dia >= 21 && $mes == 5){
+        echo "Tu signo del zodiaco es: Géminis";
+    }elseif($dia <= 20 && $mes == 6){
+        echo "Tu signo del zodiaco es: Géminis";
+    }elseif($dia >= 21 && $mes == 6){
+        echo "Tu signo del zodiaco es: Cáncer";
+    }elseif($dia <= 20 && $mes == 7){
+        echo "Tu signo del zodiaco es: Cáncer";
+    }elseif($dia >= 21 && $mes == 7){
+        echo "Tu signo del zodiaco es: Leo";
+    }elseif($dia <= 20 && $mes == 8){
+        echo "Tu signo del zodiaco es: Leo";
+    }elseif($dia >= 21 && $mes == 8){
+        echo "Tu signo del zodiaco es: Virgo";
+    }elseif($dia <= 20 && $mes == 9){
+        echo "Tu signo del zodiaco es: Virgo";
+    }elseif($dia >= 21 && $mes == 9){
+        echo "Tu signo del zodiaco es: Libra";
+    }elseif($dia <= 20 && $mes == 10){
+        echo "Tu signo del zodiaco es: Libra";
+    }elseif($dia >= 21 && $mes == 10){
+        echo "Tu signo del zodiaco es: Escorpio";
+    }elseif($dia <= 20 && $mes == 11){
+        echo "Tu signo del zodiaco es: Escorpio";
+    }elseif($dia >= 21 && $mes == 11){
+        echo "Tu signo del zodiaco es: Sagitario";
+    }elseif($dia <= 20 && $mes == 12){
+        echo "Tu signo del zodiaco es: Sagitario";
+    }elseif($dia >= 21 && $mes == 12){
+        echo "Tu signo del zodiaco es: Capricornio";
+    }elseif($dia <= 20 && $mes == 1){
+        echo "Tu signo del zodiaco es: Capricornio";
+    }elseif($dia >= 21 && $mes == 1){
+        echo "Tu signo del zodiaco es: Acuario";
+    }elseif($dia <= 20 && $mes == 2){
+        echo "Tu signo del zodiaco es: Acuario";
+    }elseif($dia >= 21 && $mes == 2){
+        echo "Tu signo del zodiaco es: Piscis";
+    }elseif($dia <= 20 && $mes == 3){
+        echo "Tu signo del zodiaco es: Piscis";
 }
 
 ?>
