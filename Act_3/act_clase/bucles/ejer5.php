dce9fddef40ea2acf018db801d44816b3b88aa28
davidroses02 <davidrosasalcaraz@gmail.com>
2021-10-11 10:23:08 +0200

    ejerCalendario

--- Act_3/act_clase/bucles/ejer5.php
+++ Act_3/act_clase/bucles/ejer5.php
@@ -6,7 +6,65 @@
      * 
      * 
      */
-
     
- 
+    $mesActual = date("F");
+
+    $fechaSistema = date('Y-m-d');
+
+    $añoSistema = intval(substr($fechaSistema, 0 , 4));
+    $diaSistema = intval(substr($fechaSistema, 8 , 9));
+
+    $meses = array (
+        "January" => 31,
+        "February"  => diaFebrero($añoSistema),
+        "March" => 31,
+        "April" => 30,
+        "May" => 31,
+        "June" => 30,
+        "July" => 31,
+        "August" => 31, 
+        "September" => 30,
+        "October" => 31,
+        "November" => 30,
+        "December" => 31,
+    );
+    
+    imprimirMes($mesActual);
+
+    echo "<table>";
+    for ($x = 1; $x <= $meses[$mesActual]; $x++) {
+        if ($x == $diaSistema) {
+            echo "<td>";
+            echo "<p style=color:green;>$x</p>";
+            echo "</td>";
+        } else {
+            if ($x == 7 || $x == 14 || $x == 21 || $x == 28) {
+                echo "<td>";
+                echo "<p style=color:red;>$x</p>";
+                echo "</td>";
+                echo "<tr>";
+            } else {
+                echo "<td>";
+                echo "<p>$x</p>";
+                echo "</td>"; 
+            }
+        }
+    }
+    
+    echo "</table>";
+    
+    function imprimirMes ($mesActual) {
+        echo "<tr><td>";
+        echo "$mesActual";
+        echo "</td></tr><br>";
+    }
+
+    function diaFebrero ($añoSistema) {
+        if ( (($añoSistema % 4) == 0) || (($añoSistema % 100) == 0) && (($añoSistema % 400) == 0) ) {
+            return 29;
+        } else {
+            return 28;
+        }
+    }
+
 ?>
