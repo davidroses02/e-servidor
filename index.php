0d67b925dff7f66f3b029476f38ab968b9be0764
davidroses02 <davidrosasalcaraz@gmail.com>
2022-03-05 19:48:53 +0100

    subiendoCositas

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>DavidRosas</title>
+</head>
+
+<body>
+    <h1>David Rosas</h1>
+    <h2>Ejercicios del Segundo Trimestre</h2>
+    <ul>
+        <li><a href="https://github.com/davidroses02/e-servidor/tree/master/trimestre_2/conexionPrac1">SuperHéroes 1</a></li>
+        <li><a href="https://github.com/davidroses02/e-servidor/tree/master/trimestre_2/conexionPrac2">SuperHéroes 2</a></li>
+        <li><a href="https://github.com/davidroses02/e-servidor/tree/master/trimestre_2/conexionPrac3">SuperHéroes - Empezando con Composer</a></li>
+        <li><a href="https://github.com/davidroses02/e-servidor/tree/master/trimestre_2/SOAP/practica_1">SOAP-1</a></li>
+        <li><a href="https://github.com/davidroses02/e-servidor/tree/master/trimestre_2/SOAP/practica_2">SOAP-2</a></li>
+        <li><a href="https://github.com/davidroses02/e-servidor/tree/master/trimestre_2/symblog-librerias">Symblog</a></li>
+        <li><a href="https://github.com/davidroses02/e-servidor/tree/master/trimestre_2/APIContactos">APIContactos</a></li>
+    </ul>
+</body>
+
+</html>
